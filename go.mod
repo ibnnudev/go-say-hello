38fa6b12a026ab2d426e463217d5cde7c49e389d
Moh Ibnu Abdurrohman Sutio <moh.ibnu@admo.astra.co.id>
2025-04-25 09:01:32 +0700

    bump module version to v3 and update Go version to 1.21.5

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Ibnnu-fwd/go-say-hello/v2
+module github.com/Ibnnu-fwd/go-say-hello/v3
 
-go 1.21.4
+go 1.21.5
--- say_hello.go
+++ say_hello.go
@@ -3,3 +3,7 @@ package gosayhello
 func SayHello(name string) string {
 	return "hello " + name
 }
+
+func CalculateSum(a, b int) int {
+	return a + b
+}
